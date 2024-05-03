46134d949c086fdfea45ef14e002a1c28976098a
BodiJu <166325840+BodiJu@users.noreply.github.com>
2024-05-03 16:15:23 +0300

    Update form.js

--- form.js
+++ form.js
@@ -2,7 +2,14 @@ const token = "6735229668:AAH0brSmSkwbv3wHSZF0pHY9oB3zbeSPL4M" // Replace with y
 const chat_id = "-1002083149793" // Replace with your group's chat ID
 const URI_API = `https://api.telegram.org/bot${token}/sendMessage`;
 
+const clearInputs = (name, contacts, age) => {
+    name.value = ''
+    contacts.value = ''
+    age.value = ''
+}
+
 document.getElementById('form').addEventListener('submit', function(event) {
+    event.preventDefault()
     let message = `<b>Имя: ${this.username.value}</b>\n`;
     message += `<b>Контакты: ${this.contacts.value}</b>\n`;
     message += `<b>Возраст: ${this.age.value}</b>\n`;
@@ -14,7 +21,7 @@ document.getElementById('form').addEventListener('submit', function(event) {
         text: message
     })
     .then(response => {
-        console.log("Message sent successfully:", response.data);
+        clearInputs(this.username, this.contacts, this.age)
     })
     .catch(error => {
         console.error("Error sending message:", error);
